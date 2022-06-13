05ee09b2db0bd0d45eff09080e664449f3e61168
Rattuti <i.ishiharar.96296@gmail.com>
2022-06-13 14:33:15 +0900

    zero

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,7 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:order_foods)
   end
 
   def exercise2
